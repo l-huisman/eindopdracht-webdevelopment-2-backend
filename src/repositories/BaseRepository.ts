97bed547c8b8d922df83d733a5ab2062fb6e6af3
Luke Huisman <huisman.l@yahoo.com>
2025-02-25 13:11:23 +0100

    Add BaseRepository class for database connection and query execution

---
+++ src/repositories/BaseRepository.ts
@@ -0,0 +1,69 @@
+import {Connection, createConnection, MysqlError} from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export class BaseRepository {
+    private readonly connection: Connection;
+
+    constructor() {
+        this.connection = createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            port: Number(process.env.DB_PORT),
+            insecureAuth: true, // The server doesn't use SSL
+        });
+    }
+
+    public openConnection(): void {
+        this.connection.connect((error: MysqlError) => {
+            if (error) {
+                console.log(`Error connecting to the database: ${error}`);
+            } else {
+                console.log("Database connected successfully");
+            }
+        });
+    }
+
+    public closeConnection(): void {
+        this.connection.end();
+    }
+
+    public async executeNonQuery(query: string, params?: any[]): Promise<void> {
+        try {
+            this.openConnection();
+            await new Promise<void>((resolve, reject) => {
+                this.connection.query(query, params, (error: any, results: any) => {
+                    if (error) {
+                        console.error(`Error executing query: ${error}`);
+                        reject(new Error("Error executing query"));
+                    } else {
+                        console.log("Query executed successfully");
+                        resolve();
+                    }
+                });
+            });
+        } finally {
+            this.closeConnection();
+        }
+    }
+
+    public async executeQuery(query: string, params?: any[]): Promise<any[]> {
+        try {
+            this.openConnection()
+            return await new Promise<any[]>((resolve, reject) => {
+                this.connection.query(query, params, (error: any, results: any) => {
+                    if (error) {
+                        reject(new Error("Error executing query"));
+                    } else {
+                        resolve(results);
+                    }
+                });
+            });
+        } finally {
+            this.closeConnection();
+        }
+    }
+}
