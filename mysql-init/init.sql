49e7c2f0bde46f7fa6aed0e71d0d9f56f20bbcba
Luke Huisman <huisman.l@yahoo.com>
2025-04-04 12:11:01 +0200

    Added a default user in the database

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -6,10 +6,9 @@ CREATE TABLE users
     username   VARCHAR(100) NOT NULL,
     email      VARCHAR(100) NOT NULL,
     password   VARCHAR(100) NOT NULL,
+    admin      tinyint DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (username, email, password)
-VALUES ('Info', 'info@lukehuisman.nl', 'password');
-INSERT INTO users (username, email, password)
-VALUES ('Dev', 'dev@lukehuisman.nl', 'password');
+INSERT INTO users (username, email, password, admin)
+VALUES ('user', 'example@lukehuisman.nl', '$2b$16$kiKuTsI3Fj5Hz.X.PIM.f.m.ApWq3ND4N4SZL7SYIJByOx6F/go3K', 1);
