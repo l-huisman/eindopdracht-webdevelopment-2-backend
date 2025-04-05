14c2d262538594a3671b2e1ea1fda7cb0d1bb0df
Luke Huisman <huisman.l@yahoo.com>
2025-04-05 16:15:35 +0200

    Add GroupRequestDTO and update initialization script; modify API response message

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -7,6 +7,7 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP DATABASE IF EXISTS `mydatabase`;
 CREATE DATABASE `mydatabase` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `mydatabase`;
 
--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import groupRoutes from './routes/groupRoutes';
 
 const routes = Router();
 
-routes.use('/v1/', (req, res) => res.json({message: 'Hello World'}));
+routes.use('/v1/', (req, res) => res.json({message: 'API is working'}));
 routes.use('/v1', userRoutes);
 routes.use('/v1', authRoutes);
 routes.use('/v1', groupRoutes);
